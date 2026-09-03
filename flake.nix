0994009cdd8048eace6ab1e05919dbdef97c85e6
Matt Innes <matt@idealogue.io>
2026-09-03 15:54:35 +0300

    Package Landline as a Nix application

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Landline development environments";
+  description = "Landline desktop application and development environments";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -7,47 +7,117 @@
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, rust-overlay, ... }:
+  outputs = { self, nixpkgs, rust-overlay, ... }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in {
-      devShells = forAllSystems (system:
+
+      systemContext = system:
         let
           pkgs = import nixpkgs {
             inherit system;
             overlays = [ rust-overlay.overlays.default ];
           };
           rust = pkgs.rust-bin.stable."1.91.0".default;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rust;
+            rustc = rust;
+          };
           landlineFonts = pkgs.google-fonts.override { fonts = [ "Inter" "Inter Tight" ]; };
           runtimeLibs = with pkgs; [
             alsa-lib
             libGL
             libxkbcommon
             wayland
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXrandr
+            libx11
+            libxcursor
+            libxi
+            libxrandr
           ];
-        in {
-          default = pkgs.mkShell {
-            packages = [
-              rust
+          landline = rustPlatform.buildRustPackage {
+            pname = "landline";
+            version = "0.1.0-preview";
+            src = ./LandlineNix;
+
+            cargoLock.lockFile = ./LandlineNix/Cargo.lock;
+
+            nativeBuildInputs = [
               pkgs.pkg-config
-              pkgs.alsa-lib
-              pkgs.libxkbcommon
-              pkgs.wayland
-              pkgs.xorg.libX11
-              pkgs.xorg.libXcursor
-              pkgs.xorg.libXi
-              pkgs.xorg.libXrandr
-              pkgs.libGL
-              landlineFonts
+              pkgs.makeWrapper
             ];
 
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
+            buildInputs = runtimeLibs;
+
             LANDLINE_FONT_DIR = "${landlineFonts}/share/fonts/truetype";
+
+            postInstall = ''
+              mv "$out/bin/landline-nix" "$out/bin/.landline-nix-unwrapped"
+              makeWrapper "$out/bin/.landline-nix-unwrapped" "$out/bin/landline" \
+                --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath runtimeLibs}"
+
+              mkdir -p "$out/share/applications"
+              cat > "$out/share/applications/landline.desktop" <<'DESKTOP'
+              [Desktop Entry]
+              Type=Application
+              Name=Landline
+              Comment=Peer-to-peer push-to-talk
+              Exec=landline
+              Terminal=false
+              Categories=Network;AudioVideo;Audio;
+              StartupNotify=true
+              DESKTOP
+            '';
+
+            meta = {
+              description = "Peer-to-peer push-to-talk desktop application";
+              homepage = "https://github.com/mattatgit/landline";
+              license = pkgs.lib.licenses.gpl3Plus;
+              mainProgram = "landline";
+              platforms = pkgs.lib.platforms.linux;
+            };
+          };
+        in {
+          inherit pkgs rust rustPlatform landlineFonts runtimeLibs landline;
+        };
+    in {
+      packages = forAllSystems (system:
+        let ctx = systemContext system;
+        in {
+          default = ctx.landline;
+          landline = ctx.landline;
+        });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.landline}/bin/landline";
+        };
+        landline = {
+          type = "app";
+          program = "${self.packages.${system}.landline}/bin/landline";
+        };
+      });
+
+      devShells = forAllSystems (system:
+        let ctx = systemContext system;
+        in {
+          default = ctx.pkgs.mkShell {
+            packages = [
+              ctx.rust
+              ctx.pkgs.pkg-config
+              ctx.pkgs.alsa-lib
+              ctx.pkgs.libxkbcommon
+              ctx.pkgs.wayland
+              ctx.pkgs.libx11
+              ctx.pkgs.libxcursor
+              ctx.pkgs.libxi
+              ctx.pkgs.libxrandr
+              ctx.pkgs.libGL
+              ctx.landlineFonts
+            ];
+
+            LD_LIBRARY_PATH = ctx.pkgs.lib.makeLibraryPath ctx.runtimeLibs;
+            LANDLINE_FONT_DIR = "${ctx.landlineFonts}/share/fonts/truetype";
             RUST_BACKTRACE = "1";
           };
         });
